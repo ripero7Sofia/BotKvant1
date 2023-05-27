0cb3db395258027a261bc739e4114996a9c70b07
ripero7Sofia <ripero7@vk.com>
2023-05-27 19:15:38 +0300

    доделать 27 мая

--- Bot.py
+++ Bot.py
@@ -1,6 +1,7 @@
 import datetime
 import vk_api
 import sqlite3
+import time
 from vk_api.longpoll import VkLongPoll, VkEventType
 
 # <Имя бота> сохрани фразу <фраза>
@@ -27,22 +28,29 @@ class VKBot:
 			if event.type == VkEventType.MESSAGE_NEW and event.to_me:
 				command_words = event.text.split(" ")
 				if command_words[0] == self.bot_name:
-					if len(command_words) > 3 and command_words[1] == "сохрани" and command_words[2] == "фразу":
-						phrase = event.text[15 + len(self.bot_name):]
-						self.conn.execute(f'INSERT OR REPLACE INTO user_info(vk_id, sentence) VALUES ({event.user_id}, "{phrase}")')
+					if len(command_words) > 3 and command_words[1] == "сохрани" and command_words[2] == "соц.сеть":
+						raspisanie = event.text[18 + len(self.bot_name):]
+						self.conn.execute(f'INSERT OR REPLACE INTO user_info(vk_id, sentence) VALUES ({event.user_id}, "{raspisanie}")')
 						self.conn.commit()
-						self.send_message(f'Фраза "{phrase}" успешно сохранена!', event.user_id)
-					if len(command_words) > 2 and command_words[1] == "напиши" and command_words[2] == "фразу":
+						self.send_message(f'Привет, cоц.сеть {raspisanie} успешно сохранена!', event.user_id)
+					if len(command_words) > 2 and command_words[1] == "напиши" and command_words[2] == "соц.сеть":
 						answer = self.conn.execute(f"SELECT sentence FROM user_info WHERE vk_id = {event.user_id}").fetchall()
 						if len(answer) > 0:
 							answer = answer[0][0]
-							self.send_message(f'Да, конечно! Вот ваша фраза: "{answer}"', event.user_id)
+							self.send_message(f'Да, конечно! Вот ваша соц.сеть: {answer}', event.user_id)
 						else:
-							self.send_message('Ошибка: вы не сохраняли никакую фразу', event.user_id)
+							self.send_message('Ошибка: вы не сохраняли никакую соц.сеть', event.user_id)
+					if len(command_words) > 2 and command_words[1] == "удали" and command_words[2] == "соц.сеть":
+								answer = self.conn.execute(f"DELETE FROM user_info WHERE vk_id = {event.user_id}").fetchall()
+								if len(answer) > 0:
+									answer = answer[0][0]
+									self.send_message(f'Да, конечно!Соц.сеть {answer} удалена ', event.user_id)
 
 
 
 
 
-bot = VKBot("KvantBot", "vk1.a.remDLGeajVP-GFKzEju_s2f63RdS659FP5PeNTJvT_st7grhlnnqiw46rasXmlNfIRdt40PvfhmrbbtjAccuVRYbREaYsH58GI32IzeiE0ieR_iW1biMm76kX23lLxXDvu8sX9gvmeiDMFcHYdzoW8QZXYCbe-TZnKeYiWe_y2tXDs1vZxnVCEUoNCmM767AAULJbfAQ8VNIeBDohmLQUw")
+
+
+bot = VKBot("Bot", "vk1.a.BE3bKi9reNDyzOhkWNRVKfyjw25hs8sfK6aiOSmnAvl7Anj7seAQjp7NLxGNYaGJJ3cj9wufgVi2zf8eq1bcoirr8W5JhUjxReMtrRodz6P3CJSQMu3lcqT1FLg383jUg7Vief7oX73WVM4jmxo_S-EbI32s5t4RaGheIpa6UhHCVDkZDvCfXPlLq-ZKdpySWX7ly1gZgHzWCe8cixhTEA")
 bot.start()
